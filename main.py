5aec21af9cbed93be2277aaecba2f51e90dcd8a6
takahashi-shu <takahashi.huyuki16@gmail.com>
2023-07-21 10:40:45 +0900

    refactoring

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-from tests import create_folder_if_not_exists,dl_video,get_chat,analyze_kusa_comments,YouTubeVideoTime,CreateClipVideo
-id = "LshIhcPr0dk"
+from tests import create_folder_if_not_exists, dl_video, get_chat, analyze_kusa_comments, YouTubeVideoTime, CreateClipVideo
 
-def main(id):
-    create_folder_if_not_exists(id)
-    get_chat(id)
-    dl_video(id)
-    top_5_minutes_list = analyze_kusa_comments(id)
-    youtube_video_time = YouTubeVideoTime()
-    creat_clip = CreateClipVideo(id)
-    i = 0
+class VideoProcessor:
+    def __init__(self, video_id: str):
+        """Initialize VideoProcessor with a specific YouTube video ID.
 
-    for item in top_5_minutes_list:
-        print( str(i)  + '  ' + item)
-        i = i + 1
-        start_time = youtube_video_time.calculate_time_elapsed(id, item)
-        creat_clip.cut_video(start_time)
-        
-id = "LshIhcPr0dk"
-main(id)
+        Args:
+            video_id: A string representing the YouTube video ID.
+        """
+        self.id = video_id
+        self.youtube_video_time = YouTubeVideoTime()
+        self.create_clip = CreateClipVideo(video_id)
+
+    def _execute_video_preparation(self):
+        """Executes a series of actions to prepare for video analysis and cutting."""
+        try:
+            create_folder_if_not_exists(self.id)
+            get_chat(self.id)
+            dl_video(self.id)
+        except Exception as e:
+            raise Exception(f"Failed to prepare video {self.id}. Reason: {str(e)}")
+
+    def process_top_comments(self):
+        """Analyzes the top comments of the video and cuts the related video segments."""
+        try:
+            top_5_minutes_list = analyze_kusa_comments(self.id)
+
+            for i, item in enumerate(top_5_minutes_list):
+                print(f"{i}  {item}")
+                start_time = self.youtube_video_time.calculate_time_elapsed(self.id, item)
+                self.create_clip.cut_video(start_time)
+        except Exception as e:
+            raise Exception(f"Failed to process top comments for video {self.id}. Reason: {str(e)}")
+
+def main(video_id: str):
+    """Main function to execute the video processing.
+
+    Args:
+        video_id: A string representing the YouTube video ID.
+    """
+    processor = VideoProcessor(video_id)
+    processor._execute_video_preparation()
+    processor.process_top_comments()
+
+if __name__ == "__main__":
+    video_id = "LshIhcPr0dk"
+    main(video_id)
